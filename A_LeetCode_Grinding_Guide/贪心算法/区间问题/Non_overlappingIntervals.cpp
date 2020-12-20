07cbe136842c94eb1ca49f9d73f9257ed8d56df1
HoSingWong <59410538+HoSingWong@users.noreply.github.com>
2020-12-20 22:45:40 +0800

    Create Non_overlappingIntervals.cpp

---
+++ A_LeetCode_Grinding_Guide/贪心算法/区间问题/Non_overlappingIntervals.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include <algorithm>//使用sort要有这个头文件
+#include<vector>
+using namespace std;
+class Non_overlappingIntervals {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if (intervals.empty()) {
+            return 0;
+        }
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b) {
+            return a[1] < b[1];
+        });
+        int total = 0, prev = intervals[0][1];
+        for (int i = 1; i < n; ++i) {
+            if (intervals[i][0] < prev) {
+                ++total;
+            } 
+            else {
+                prev = intervals[i][1];
+            }
+        }
+        return total;
+    }
+};
+
+int main(){
+    Non_overlappingIntervals result;
+    vector<vector<int>> intervals={{1,2}};
+    cout<<result.eraseOverlapIntervals(intervals)<<endl;
+    return 0;
+}
